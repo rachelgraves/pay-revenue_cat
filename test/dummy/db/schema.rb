06af65ef5738be73a794617627ae940f781da137
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-03-28 15:23:46 +0000

    Prepare for tests

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -109,6 +109,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_28_150903) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,3 +14,18 @@ ActiveRecord::Migrator.migrations_paths = [
 ]
 require "rails/test_help"
 Rails.backtrace_cleaner.remove_silencers!
+
+# Filter out Minitest backtrace while allowing backtrace from other libraries
+# to be shown.
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  ActiveSupport::TestCase.fixture_paths << File.expand_path("fixtures", __dir__)
+  ActionDispatch::IntegrationTest.fixture_paths << File.expand_path("fixtures", __dir__)
+elsif ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+end
+ActiveSupport::TestCase.file_fixture_path = File.expand_path("fixtures/files", __dir__)
+ActiveSupport::TestCase.fixtures :all

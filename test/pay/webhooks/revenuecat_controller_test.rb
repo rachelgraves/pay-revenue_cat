1691832626504164446d2eae7a67733c784806e7
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-04-03 16:24:46 +0100

    Add integration tests

--- test/pay/webhooks/revenuecat_controller_test.rb
+++ test/pay/webhooks/revenuecat_controller_test.rb
@@ -7,6 +7,7 @@ module Pay
 
     def setup
       @pay_user = users(:revenuecat)
+      @pay_customer = pay_customers(:revenuecat)
     end
 
     test "should handle INITIAL_PURCHASE event" do
@@ -16,32 +17,57 @@ module Pay
           assert_response :success
         end
       end
+
+      assert_difference "Pay::Charge.count" do
+        perform_enqueued_jobs
+      end
     end
 
     test "should handle RENEWAL event" do
+      subscription = create_subscription(initial_purchase_params)
+      create_initial_charge(initial_purchase_params, subscription)
+
       assert_difference "Pay::Webhook.count" do
         assert_enqueued_with(job: Pay::Webhooks::ProcessJob) do
           post webhooks_revenuecat_path, params: renewal_params, as: :json
           assert_response :success
         end
       end
+
+      assert_difference "Pay::Charge.count" do
+        perform_enqueued_jobs
+      end
     end
 
     test "should handle CANCELLATION event" do
+      subscription = create_subscription(initial_purchase_params)
+      create_initial_charge(initial_purchase_params, subscription)
+
       assert_difference "Pay::Webhook.count" do
         assert_enqueued_with(job: Pay::Webhooks::ProcessJob) do
           post webhooks_revenuecat_path, params: cancellation_params, as: :json
           assert_response :success
         end
       end
+
+      assert_changes -> { subscription.reload.status }, to: "canceled" do
+        perform_enqueued_jobs
+      end
     end
 
     test "should handle EXPIRATION event" do
+      subscription = create_subscription(initial_purchase_params)
+      create_initial_charge(initial_purchase_params, subscription)
+
       assert_difference "Pay::Webhook.count" do
         assert_enqueued_with(job: Pay::Webhooks::ProcessJob) do
           post webhooks_revenuecat_path, params: expiration_params, as: :json
           assert_response :success
         end
+      end
+
+      assert_changes -> { subscription.reload.status }, to: "canceled" do
+        perform_enqueued_jobs
       end
     end
     # test "should handle revenuecat post requests" do
@@ -65,6 +91,27 @@ module Pay
     # end
 
     private
+
+    def create_subscription(payload)
+      Pay::Revenuecat::Subscription.create!(
+        name: "todo: Figure out what should go here",
+        processor_plan: payload["event"]["product_id"],
+        processor_id: payload["event"]["original_transaction_id"],
+        current_period_start: 27.days.ago,
+        current_period_end: 1.month.from_now.beginning_of_month,
+        status: :active,
+        customer: @pay_customer
+      )
+    end
+
+    def create_initial_charge(payload, subscription)
+      Pay::Revenuecat::Charge.create!(
+        subscription: subscription,
+        processor_id: payload["event"]["transaction_id"],
+        amount: 9.99,
+        customer: @pay_customer
+      )
+    end
 
     def initial_purchase_params
       JSON.parse(

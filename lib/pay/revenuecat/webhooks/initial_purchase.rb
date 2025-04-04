ef36c87e36d61476e228b0eba98f1da78b8609d7
Rachel Graves <83824730+rachelgraves@users.noreply.github.com>
2025-04-04 11:50:34 +0100

    Remove comments
    I have decided not to worry about storing / handling the monetary
    information coming back from revenuecat right now.
    
    This decision is inspired by Joe Masilotti's minimal configuration on
    RailsDevs
    
    https://github.com/joemasilotti/railsdevs.com/blob/b05ee6e886811c45d4bde77f6a99973401f45e62/test/models/pay/subscription_changes_test.rb#L68-L78

--- lib/pay/revenuecat/webhooks/initial_purchase.rb
+++ lib/pay/revenuecat/webhooks/initial_purchase.rb
@@ -17,23 +17,18 @@ module Pay
           args = {
             name: event["presented_offering_id"],
             plan: event["product_id"],
-            processor_id: event["original_transaction_id"], # seems to be the closest thing I can find to a subscription id across all events
+            processor_id: event["original_transaction_id"],
             current_period_start: Time.at(event["purchased_at_ms"].to_i / 1000),
             current_period_end: Time.at(event["expiration_at_ms"].to_i / 1000),
             ends_at: Time.at(event["expiration_at_ms"].to_i / 1000),
             metadata: event["metadata"],
             data: data,
-            metered: false, # TODO: Handle metered billing
+            metered: false,
             status: :active # TODO: set "on_trial", "active", "canceled"
-            # application_fee_percent: nil,
-            # application_fee_percent: — stripe send this, though is null in all fixtures, revenue cat send:
-            #  "tax_percentage"=>0.1705,
-            #  "commission_percentage"=>0.2489,
-            # "is_trial_conversion"=>false, # TODO: do something with this?
           }
 
           payment_processor = pay_customer.owner.payment_processor
-          payment_processor.subscribe(**args) # TODO: with lock?
+          payment_processor.subscribe(**args)
 
           Pay::Revenuecat::Charge.create!(
             processor_id: event["transaction_id"],
